28ce0e017742f119d03ae54f367834d0b2a2898b
Aleksandar Lazarevic <aleksandar.lazarevic90@gmail.com>
2024-12-10 21:25:20 +0100

    Add basic routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'component-a',
+    loadComponent: () => import('./features/home/components/component-a/component-a.component').then((c) => c.ComponentAComponent)
+  },
+  {
+    path: 'component-b',
+    loadComponent: () => import('./features/home/components/component-b/component-b.component').then((c) => c.ComponentBComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./core/components/page-not-found/page-not-found.component').then((c) => c.PageNotFoundComponent)
+  }
+];
